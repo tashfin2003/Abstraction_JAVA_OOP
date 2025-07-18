9c19358205c1a48cbfc612254936ba8ee5d33ece
tashfin2003 <tashfin514@gmail.com>
2025-07-18 14:29:34 +0600

    first commit

---
+++ src/Engine.java
@@ -0,0 +1,38 @@
+public interface Engine {
+    void start();
+    void stop();
+    void crashedCar();
+}
+
+abstract class Car{
+    void carModel(){
+        System.out.println("Old Model Car");
+    }
+    void carCondition(){
+        System.out.println("Good Condition");
+    }
+    abstract void shopName();
+    abstract void location();
+}
+
+class Tashfin extends Car implements Engine{
+    public void start(){
+        System.out.println("Car Starts");
+    }
+    public void stop(){
+        System.out.println("Car Stops");
+    }
+    public void crashedCar(){
+        System.out.println("Car Crashed");
+    }
+
+    @Override
+    void shopName(){
+        System.out.println("Kuddus Car Shop");
+    }
+    @Override
+    void location(){
+        System.out.println("Mirpur-1, Dhaka 1216");
+    }
+
+}
---
+++ src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        Tashfin myCar = new Tashfin();
+
+        myCar.start();
+        myCar.stop();
+        myCar.crashedCar();
+        myCar.carModel();
+        myCar.carCondition();
+        myCar.shopName();
+        myCar.location();
+    }
+}
